d65f3b1753fd4bac7a607077d391ddee22dd36c1
DBC Apprentice <apprentice@devbootcamp.com>
2012-11-21 18:56:45 -0800

    Working ajax on comments

--- Gemfile
+++ Gemfile
@@ -27,6 +27,7 @@ gem 'jquery-rails'
 group :test, :development do
   gem 'rspec-rails', '~> 2.0'
   gem 'faker'
+  gem 'quiet_assets'
 end
 
 # To use ActiveModel has_secure_password
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,16 +1,16 @@
 class CommentsController < ApplicationController
   # POST /comments
   # POST /comments.json
-  
+
   before_filter :censorship, :only => [:create]
-  
+
   def create
 
     respond_to do |format|
       if @comment.save
         # format.html { redirect_to @comment.article, notice: 'Comment was successfully created.' }
         # format.json { render json: @comment, status: :created, location: @comment }
-        format.js 
+        format.js
       else
         format.html { redirect_to @comment.article }
         format.json { render json: @comment.errors, status: :unprocessable_entity }
@@ -29,13 +29,13 @@ class CommentsController < ApplicationController
       format.json { head :no_content }
     end
   end
-  
+
   private
-  
+
   def censorship
     @article = Article.find(params[:article_id])
     @comment = @article.comments.build(params[:comment])
     @comment.body.gsub!(/sad/, "happy")
   end
-  
+
 end
