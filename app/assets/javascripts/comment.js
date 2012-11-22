dc525b2dbafc452f74fba67b190627c7da27c7ab
Phil Moldavski <phil.moldavski@gmail.com>
2012-11-21 18:28:52 -0800

    Half working Ajax on comments

---
+++ app/assets/javascripts/comment.js
@@ -0,0 +1,5 @@
+// $(document).ready(function(){	
+// $('?').on('ajax:success', 'form', function(event, data) {
+// $('?').append(data.comment);
+// });
+// };
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,8 +8,9 @@ class CommentsController < ApplicationController
 
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to @comment.article, notice: 'Comment was successfully created.' }
-        format.json { render json: @comment, status: :created, location: @comment }
+        # format.html { redirect_to @comment.article, notice: 'Comment was successfully created.' }
+        # format.json { render json: @comment, status: :created, location: @comment }
+        format.js 
       else
         format.html { redirect_to @comment.article }
         format.json { render json: @comment.errors, status: :unprocessable_entity }
